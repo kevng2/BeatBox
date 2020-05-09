962f528376ac07294d49bd50abe8924c4c1eefb7
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-05-09 16:44:47 -0400

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.beatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_beat_box);
+    }
+}
