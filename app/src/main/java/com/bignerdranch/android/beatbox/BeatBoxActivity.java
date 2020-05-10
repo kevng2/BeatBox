355ab1988161c84dfd54182ff50259f415e06a86
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-05-10 10:23:27 -0400

    added SingleFragementActivity and edited the build.gradle to support data binding

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -1,14 +1,9 @@
 package com.bignerdranch.android.beatbox;
+import androidx.fragment.app.Fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class BeatBoxActivity extends AppCompatActivity {
-
+public class BeatBoxActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beat_box);
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.beatbox;
+
+import androidx.fragment.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
